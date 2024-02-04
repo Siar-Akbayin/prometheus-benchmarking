f0c7123cbee1a60f9fc436f10b1e398b33bd929e
Siar <siarakbayin@gmail.com>
2024-02-04 01:04:56 +0100

    Added warmup phase to benchmarking client

--- benchmark.go
+++ benchmark.go
@@ -14,25 +14,23 @@ import (
 	"time"
 )
 
-// Experiment structure
 type Experiment struct {
 	Queries                  []string `json:"queries"`
 	QueryRatePerSecond       int      `json:"queryRatePerSecond"`
 	BenchmarkDurationSeconds int      `json:"benchmarkDurationSeconds"`
 	NumberOfUsers            int      `json:"numberOfUsers"`
 	MaxCardinality           int      `json:"maxCardinality"`
+	Iterations               int      `json:"iterations"`
 }
 
-// Config structure
 type Config struct {
 	PrometheusServer string       `json:"prometheusServer"`
 	Experiments      []Experiment `json:"experiments"`
 }
 
-// queryPrometheus function
+// Queries Prometheus and returns the query duration in milliseconds
 func queryPrometheus(query string, prometheusServer string) (float64, error) {
 	queryURL := fmt.Sprintf("%s/api/v1/query?query=%s", prometheusServer, query)
-
 	startTime := time.Now()
 	response, err := http.Get(queryURL)
 	if err != nil {
@@ -44,11 +42,10 @@ func queryPrometheus(query string, prometheusServer string) (float64, error) {
 		return 0, err
 	}
 	duration := time.Since(startTime).Seconds()
-
-	return duration * 1000, nil
+	return duration * 10000, nil
 }
 
-// constructQueryWithCardinality function
+// Constructs a query with a specific cardinality
 func constructQueryWithCardinality(baseQuery string, cardinality int) string {
 	var labelFilters []string
 	for i := 0; i < cardinality; i++ {
@@ -58,8 +55,40 @@ func constructQueryWithCardinality(baseQuery string, cardinality int) string {
 	return fmt.Sprintf(`sample_gauge_%d{%s}`, cardinality, strings.Join(labelFilters, ","))
 }
 
+// Warmup function that performs queries without saving results
+func warmup(config Config, experiment Experiment) {
+	fmt.Println("Starting warmup period...")
+	warmupDuration := 60 // Warmup duration in seconds
+	stopWarmup := time.After(time.Duration(warmupDuration) * time.Second)
+	var warmupTicker *time.Ticker
+	if experiment.QueryRatePerSecond == -1 {
+		// Use a very short duration for maximizing the rate
+		warmupTicker = time.NewTicker(1 * time.Microsecond)
+	} else {
+		// Use the specified query rate
+		warmupTicker = time.NewTicker(time.Second / time.Duration(experiment.QueryRatePerSecond))
+	}
+	defer warmupTicker.Stop()
+
+WarmupLoop:
+	for {
+		select {
+		case <-warmupTicker.C:
+			for _, query := range experiment.Queries {
+				queryWithCardinality := constructQueryWithCardinality(query, experiment.MaxCardinality)
+				_, err := queryPrometheus(queryWithCardinality, config.PrometheusServer)
+				if err != nil {
+					log.Printf("Warmup query error: %s", err)
+				}
+			}
+		case <-stopWarmup:
+			break WarmupLoop
+		}
+	}
+	fmt.Println("Warmup complete.")
+}
+
 func main() {
-	// Read configuration
 	configFile, err := os.Open("config.json")
 	if err != nil {
 		log.Fatal("Could not open config file", err)
@@ -74,83 +103,85 @@ func main() {
 	}
 
 	for _, experiment := range config.Experiments {
-		// Construct the filename based on experiment parameters
-		filename := fmt.Sprintf("query_benchmark_results_%dreqs_%dsecs_%dusers_%dcard.csv",
-			experiment.QueryRatePerSecond, experiment.BenchmarkDurationSeconds,
-			experiment.NumberOfUsers, experiment.MaxCardinality)
+		// Perform warmup for each experiment
+		warmup(config, experiment)
 
-		// Open file for writing
-		file, err := os.Create(filename)
-		if err != nil {
-			log.Fatal("Could not create CSV file", err)
-		}
-		defer file.Close()
+		for iteration := 1; iteration <= experiment.Iterations; iteration++ {
+			filename := fmt.Sprintf("query_benchmark_results_%dreqs_%dsecs_%dusers_%dcard_%d.csv",
+				experiment.QueryRatePerSecond, experiment.BenchmarkDurationSeconds,
+				experiment.NumberOfUsers, experiment.MaxCardinality, iteration)
 
-		// Write CSV header
-		writer := csv.NewWriter(file)
-		defer writer.Flush()
-		header := []string{"Timestamp", "Cardinality", "Duration (Milliseconds)"}
-		writer.Write(header)
+			file, err := os.Create(filename)
+			if err != nil {
+				log.Fatalf("Could not create CSV file: %v", err)
+			}
+			defer file.Close()
 
-		// Channel and WaitGroup for goroutines
-		results := make(chan [3]string)
-		var wg sync.WaitGroup
+			writer := csv.NewWriter(file)
+			defer writer.Flush()
 
-		// Adjust queries based on cardinality - updated to use new query format
-		for i, _ := range experiment.Queries {
-			experiment.Queries[i] = constructQueryWithCardinality("sample_gauge", experiment.MaxCardinality)
-		}
+			header := []string{"Timestamp", "Cardinality", "Latency (Deci-milliseconds)", "Users", "Duration", "Failure"}
+			writer.Write(header)
 
-		// Start benchmark
-		stopBenchmark := make(chan struct{})
-		go func() {
-			time.Sleep(time.Duration(experiment.BenchmarkDurationSeconds) * time.Second)
-			close(stopBenchmark)
-		}()
+			results := make(chan [6]string)
+			var wg sync.WaitGroup
 
-		for i := 0; i < experiment.NumberOfUsers; i++ {
-			wg.Add(1)
+			stopBenchmark := make(chan struct{})
 			go func() {
-				defer wg.Done()
-				var ticker *time.Ticker
-				if experiment.QueryRatePerSecond == -1 {
-					ticker = time.NewTicker(1 * time.Nanosecond)
-				} else {
-					ticker = time.NewTicker(time.Second / time.Duration(experiment.QueryRatePerSecond))
-				}
-				defer ticker.Stop()
-
-				for {
-					select {
-					case <-ticker.C:
-						for _, query := range experiment.Queries {
-							startTime := time.Now()
-							duration, err := queryPrometheus(query, config.PrometheusServer)
-							if err != nil {
-								log.Printf("Error querying Prometheus: %s", err)
-								continue
-							}
-							timestamp := startTime.Format(time.RFC3339)
-							results <- [3]string{timestamp, strconv.Itoa(experiment.MaxCardinality), fmt.Sprintf("%.0f", duration)}
-						}
-					case <-stopBenchmark:
-						return
-					}
-				}
+				time.Sleep(time.Duration(experiment.BenchmarkDurationSeconds) * time.Second)
+				close(stopBenchmark)
 			}()
+
+			for i := 0; i < experiment.NumberOfUsers; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					var ticker *time.Ticker
+					if experiment.QueryRatePerSecond == -1 {
+						ticker = time.NewTicker(1 * time.Nanosecond)
+					} else {
+						ticker = time.NewTicker(time.Second / time.Duration(experiment.QueryRatePerSecond))
+					}
+					defer ticker.Stop()
+
+					for {
+						select {
+						case <-ticker.C:
+							for _, query := range experiment.Queries {
+								queryWithCardinality := constructQueryWithCardinality(query, experiment.MaxCardinality)
+								startTime := time.Now()
+								duration, err := queryPrometheus(queryWithCardinality, config.PrometheusServer)
+								failure := "0"
+								if err != nil {
+									log.Printf("Error querying Prometheus: %s, Number of users: %d, Cardinality: %d", err, experiment.NumberOfUsers, experiment.MaxCardinality)
+									failure = "1"
+								}
+								timestamp := startTime.Format(time.RFC3339)
+								results <- [6]string{timestamp, strconv.Itoa(experiment.MaxCardinality), fmt.Sprintf("%.0f", duration), strconv.Itoa(experiment.NumberOfUsers), strconv.Itoa(experiment.BenchmarkDurationSeconds), failure}
+							}
+						case <-stopBenchmark:
+							return
+						}
+					}
+				}()
+			}
+
+			go func() {
+				wg.Wait()
+				close(results)
+			}()
+
+			for result := range results {
+				writer.Write(result[:])
+			}
+
+			fmt.Printf("Query benchmark results for iteration %d saved to %s\n", iteration, filename)
 		}
-
-		// Collecting results
-		go func() {
-			wg.Wait()
-			close(results)
-		}()
-
-		// Write results to CSV
-		for result := range results {
-			writer.Write(result[:])
-		}
-
-		fmt.Printf("Query benchmark results saved to %s\n", filename)
 	}
+
+	flagFilePath := "benchmark_complete.flag"
+	if _, err := os.Create(flagFilePath); err != nil {
+		log.Fatalf("Failed to create benchmark completion flag file: %v", err)
+	}
+	fmt.Println("Benchmarking complete. Flag file created.")
 }
